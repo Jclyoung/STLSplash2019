e189e731c5f9ea53f73587c41ac5f2114f999d42
JCLYoung <jclyoung@gmail.com>
2019-07-08 09:28:10 -0500

    Models

---
+++ Models/Location.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace STLSplash2019.Models
+{
+    public class Location
+    {
+        public int Id { get; set; }        
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Area { get; set; }
+        public int Description { get; set; }
+        public List<LocationRating> Ratings { get; set; }
+        public IList<LocationCategory> LocationCategories { get; set; }
+    }
+}
---
+++ Models/LocationCategory.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace STLSplash2019.Models
+{
+    public class LocationCategory
+    {
+        public int Id { get; set; }
+        public int LocationId { get; set; }
+        public Location Location { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
